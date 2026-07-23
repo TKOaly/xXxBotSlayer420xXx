db3c9ade22144434a2c88063eeb5bfa39fabdb4f
Eetu Raunio <eetu.raunio@helsinki.fi>
2026-07-23 13:14:32 +0300

    fix: migrate chat to new supergroup id on upgrade res
    fixes #10

--- index.ts
+++ index.ts
@@ -142,15 +142,16 @@ for (const { id, name } of allChats) {
   } catch (e) {
     if (e instanceof Error) {
       if (e.message.includes("chat not found")) {
-        console.info(`[👻] Chat ${chatDetails} not found. Removed.`);
+        console.info(`[👻] Chat ${chatDetails} not found`);
         chats.deleteChat(id);
         continue;
       }
 
       if (e.message.includes("chat was upgraded to a supergroup")) {
-        console.info(`[⏫] Chat ${chatDetails} upgraded to supergroup`);
-        console.info(util.inspect(e, { depth: 4 }));
-        console.info(`[⏫] No action taken yet!`);
+        const newId = e.response?.body?.parameters?.migrate_to_chat_id;
+        console.info(`[⏫] Chat ${chatDetails} upgraded to supergroup, new id ${newId}. Admin status will be checked when needed`);
+        chats.deleteChat(id);
+        chats.addChat(newId);
         continue;
       }
     }
